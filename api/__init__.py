6ef978c67a51038a6d023be16737736208f84d80
matthieu <matthieu.gouel@gmail.com>
2018-02-05 22:25:02 +0100

    Move Factory class into a factory module

--- api/__init__.py
+++ api/__init__.py
@@ -1,15 +1,9 @@
 """Initialization module of the package."""
-# Flask based imports
-from flask import Flask
-
-# Plugins based imports
-
+# API Factory imports
+from api.factory import Factory
 
 # API configuration imports
-from api.config import Config, config
-
-# System based imports
-import os
+from api.config import Config
 
 # Version handling
 import pkg_resources
@@ -22,40 +16,6 @@ except pkg_resources.DistributionNotFound:  # pragma: no cover
     # If app is not used as a package
     # Hardcode the version from the configuration file
     __version__ = Config.VERSION
-
-
-class Factory(object):
-    """Build the instances needed for the API."""
-
-    def __init__(self, environment='default'):
-        """Initialize Factory with the proper environment."""
-        # Get the running environment
-        self._environment = os.getenv("APP_ENVIRONMENT")
-        if not self._environment:
-            self._environment = environment
-
-    @property
-    def environment(self):
-        """Getter for environment attribute."""
-        return self._environment
-
-    def set_flask(self, **kwargs):
-        """Flask instantiation."""
-        # Flask instance creation
-        self.flask = Flask(__name__, **kwargs)
-
-        # Flask configuration
-        self.flask.config.from_object(config[self._environment])
-
-        # Swagger documentation
-        self.flask.config.SWAGGER_UI_DOC_EXPANSION = 'list'
-        self.flask.config.SWAGGER_UI_JSONEDITOR = True
-
-        return self.flask
-
-    def register(self, blueprint):
-        """Register a specified blueprint."""
-        self.flask.register_blueprint(blueprint)
 
 
 # Instantiation of the factory
